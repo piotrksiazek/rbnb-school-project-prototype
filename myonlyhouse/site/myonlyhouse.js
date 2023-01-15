ea07b24d0028e16bf3bf0e300d35101b2055385b
Alex <36136484+homosum1@users.noreply.github.com>
2023-01-15 15:57:53 +0100

    obsługa formularzy - etap 1

--- myonlyhouse/site/myonlyhouse.js
+++ myonlyhouse/site/myonlyhouse.js
@@ -1,80 +1,111 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const multiparty = require('multiparty');
 const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
 const database = require('./dbsqlite3');
 
-
-const handlers = require('./src/lib/handlers');
+const getHandlers = require('./src/lib/get_handlers');
 
 const { credentials } = require('./src/config');
 
 const app = express();
 
 // handlebars configuration
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.use(express.static(__dirname + '/public'));
+app.engine(
+	'handlebars',
+	expressHandlebars.engine({
+		defaultLayout: 'main',
+		layoutsDir: `${__dirname}/views/layouts`,
+		partialsDir: `${__dirname}/views/partials`,
+	})
+);
 
-// body parser configuration
-app.use(bodyParser.json());
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 // cookie-parser secret set
 app.use(cookieParser(credentials.cookieSecret));
 
 // express-session init
-app.use(expressSession({
-    resave: false,
-    saveUninitialized: false,
-    secret: credentials.cookieSecret,
-}));
-
+app.use(
+	expressSession({
+		resave: false,
+		saveUninitialized: false,
+		secret: credentials.cookieSecret,
+	})
+);
 
 const port = process.env.PORT || 3000;
 
-
-
 // main websites
-app.get('/', handlers.home);
-app.get('/search_results', handlers.search_results);
-app.get('/offer_preview', handlers.offer_preview);
-app.get('/confirmation', handlers.confirmation);
-app.get('/confirmation_sent', handlers.confirmation_sent);
-app.get('/contact', handlers.contact);
-app.get('contact_sent', handlers.contact_sent)
-app.get('/report', handlers.report);
-app.get('/report_sent', handlers.report_sent);
-app.get('/reservations', handlers.reservations);
-app.get('/accommodation_report', handlers.accommodation_report);
-app.get('/reservations', handlers.reservations);
-app.get('/accommodation_report_sent', handlers.accommodation_report_sent);
-app.get('/offer_deleted', handlers.offer_deleted);
+app.get('/', getHandlers.home);
+app.get('/search_results', getHandlers.search_results);
+app.get('/offer_preview', getHandlers.offer_preview);
+app.get('/confirmation', getHandlers.confirmation);
+app.get('/confirmation_sent', getHandlers.confirmation_sent);
+app.get('/contact', getHandlers.contact);
+app.get('contact_sent', getHandlers.contact_sent);
+app.get('/report', getHandlers.report);
+app.get('/report_sent', getHandlers.report_sent);
+app.get('/reservations', getHandlers.reservations);
+app.get('/accommodation_report', getHandlers.accommodation_report);
+app.get('/reservations', getHandlers.reservations);
+app.get('/accommodation_report_sent', getHandlers.accommodation_report_sent);
+app.get('/offer_deleted', getHandlers.offer_deleted);
 
 // login - registration
-app.get('/login', handlers.login);
-app.get('/registration', handlers.registration);
-app.get('/account_created ', handlers.account_created );
-
+app.get('/login', getHandlers.login);
+app.get('/registration', getHandlers.registration);
+app.get('/account_created ', getHandlers.account_created);
 
 // process forms
-/* ... */
+app.post('/home', (req, res) => {
+	try {
+		console.log('req body: ');
+		console.log(req.body);
+		// gdy się uda
+		res.redirect('/search_results');
+	} catch (err) {
+		res.status(404).json({
+			status: 'fail',
+			message: err,
+		});
+	}
+});
+
+
+
+// ERRORS
+const notFound = (req, res) => {
+	res.render('404');
+};
+
+const serverError = (err, req, res, next) => {
+	res.render('500');
+};
 
 // strona 404
-app.use(handlers.notFound);
-
+app.use(notFound);
 // strona 500
-app.use(handlers.serverError);
+app.use(serverError);
 
 
 if (require.main === module) {
-    app.listen(port, () => console.log(
-        `Express started at http://localhost:${port}; ` +
-        'Press Ctrl-C in order to stop.'
-    ));
+	app.listen(port, () =>
+		console.log(
+			`Express started at http://localhost:${port}; ` +
+				'Press Ctrl-C in order to stop.'
+		)
+	);
+} else {
+	module.exports = app;
 }
-else {
-    module.exports = app;
-}
-
