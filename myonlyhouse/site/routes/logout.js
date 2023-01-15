0b68a27bd93af78cec644e30c3ba047c4fc98e35
Alex <36136484+homosum1@users.noreply.github.com>
2023-01-15 23:09:05 +0100

    cosmetics

--- myonlyhouse/site/routes/logout.js
+++ myonlyhouse/site/routes/logout.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const db = require('../dbsqlite3')
+const express = require('express');
+
+const router = express.Router();
+const db = require('../dbsqlite3');
 
 router.get('/', (req, res) => {
-    req.session.destroy()
-    res.send("Pomyslnie wylogowano")
-})
+	req.session.destroy();
+	res.send('Pomyslnie wylogowano');
+});
 
-module.exports = router
+module.exports = router;
--- myonlyhouse/site/src/lib/handlers.js
+++ myonlyhouse/site/src/lib/handlers.js
@@ -1,93 +1,90 @@
 exports.home = (req, res) => {
-    res.render('home');
-}
+	res.render('home');
+};
 
 // app pages
 
 exports.offer_preview = (req, res) => {
-    res.render('offer_preview');
-}
+	res.render('offer_preview');
+};
 
 exports.search_results = (req, res) => {
-    res.render('search_results');
-}
+	res.render('search_results');
+};
 
 exports.confirmation = (req, res) => {
-    res.render('confirmation');
-}
+	res.render('confirmation');
+};
 
 exports.confirmation_sent = (req, res) => {
-    res.render('confirmation_sent');
-}
+	res.render('confirmation_sent');
+};
 
 exports.contact = (req, res) => {
-    res.render('contact');
-}
+	res.render('contact');
+};
 
 exports.contact_sent = (req, res) => {
-    res.render('contact_sent');
-}
+	res.render('contact_sent');
+};
 
 exports.report = (req, res) => {
-    res.render('report');
-}
+	res.render('report');
+};
 
 exports.report_sent = (req, res) => {
-    res.render('report_sent');
-}
+	res.render('report_sent');
+};
 
 exports.reservations = (req, res) => {
-    res.render('reservations');
-}
+	res.render('reservations');
+};
 
 exports.accommodation_report = (req, res) => {
-    res.render('accommodation_report');
-}
+	res.render('accommodation_report');
+};
 
 exports.accommodation_report_sent = (req, res) => {
-    res.render('accommodation_report_sent');
-}
+	res.render('accommodation_report_sent');
+};
 
 exports.my_offers = (req, res) => {
-    res.render('my_offers');
-}
+	res.render('my_offers');
+};
 
 exports.offer_deleted = (req, res) => {
-    res.render('offer_deleted');
-}
+	res.render('offer_deleted');
+};
 
 // forms
 /* ... */
 
-
 // login - registration
 
 exports.login = (req, res) => {
-    res.render('login');
-}
+	res.render('login');
+};
 
 exports.registration = (req, res) => {
-    res.render('registration');
-}
+	res.render('registration');
+};
 
 exports.account_created = (req, res) => {
-    res.render('account_created');
-}
-
+	res.render('account_created');
+};
 
 // errors
 
 exports.notFound = (req, res) => {
-    res.render('404');
-}
+	res.render('404');
+};
 
 /* eslint-disable no-unused-vars */
 exports.serverError = (err, req, res, next) => {
-    res.render('500');
-}
+	res.render('500');
+};
 /* eslint-enable no-unused-vars */
 
-
 exports.account = (req, res) => {
-    res.render('account');
-}
+	res.render('account');
+};
